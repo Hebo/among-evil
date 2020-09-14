b3496bfd04d92504d0437aeb30ada832aa19a2ba
Hebo <hebo@hey.com>
2020-09-14 10:55:15 -0700

    v0.1

---
+++ src/App.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const ready = function (cb: any) {
+  // Check if the `document` is loaded completely
+  document.readyState === "loading"
+    ? document.addEventListener("DOMContentLoaded", function (e) {
+        cb();
+      })
+    : cb();
+};
+
+let switchStatus = (el: any, reset = false) => {
+  let statusAlive = ["is-success", "is-light"];
+  let statusDead = "is-dark";
+
+  let alive = el.classList.contains(statusAlive[0]);
+  if (!alive || reset) {
+    el.classList.remove(statusDead);
+    el.classList.add(...statusAlive);
+    el.textContent = "Alive";
+  } else {
+    el.classList.add(statusDead);
+    el.classList.remove(...statusAlive);
+    el.textContent = "Dead";
+  }
+};
+
+ready(function () {
+  let elems = document.querySelectorAll(".js-player");
+  elems.forEach((el) => {
+    let scoreEle = el.querySelector(".js-score");
+    let statusEl = el.querySelector(".js-status");
+    if (scoreEle === null || statusEl === null) {
+      return;
+    }
+
+    el.querySelector(".good")!.addEventListener("click", () => {
+      scoreEle!.innerHTML = (parseInt(scoreEle!.innerHTML) + 1).toString();
+    });
+
+    el.querySelector(".bad")!.addEventListener("click", () => {
+      scoreEle!.innerHTML = (parseInt(scoreEle!.innerHTML) - 1).toString();
+    });
+
+    scoreEle!.addEventListener("click", () => {
+      scoreEle!.innerHTML = "0";
+    });
+
+    statusEl.addEventListener("click", (e) => {
+      switchStatus(e.target);
+    });
+  });
+});
+
+function App() {
+  return (
+    <div className="App">
+      <ResetButton />
+    </div>
+  );
+}
+
+const ResetButton = () => {
+  const resetAll = () => {
+    document.querySelectorAll(".js-score").forEach((e) => {
+      e.innerHTML = "0";
+    });
+
+    document.querySelectorAll(".js-status").forEach((e) => {
+      switchStatus(e, true);
+      return;
+    });
+  };
+
+  return (
+    <button
+      className="button is-warning is-light"
+      id="reset"
+      onClick={resetAll}
+    >
+      Reset Scores
+    </button>
+  );
+};
+
+export default App;
