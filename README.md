d97625f8d1df61a68298576fafe316b693cf8362
Hebo <hebo@hey.com>
2020-09-15 15:44:24 -0700

    Migrating to react. This was a mistake

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 Clearly, I have no better things to be doing.
 
 See me [here](https://hebo.github.io/among-evil)
+
+TODO
+-----
+
+[ ] Improve breakpoints
--- src/App.tsx
+++ src/App.tsx
@@ -1,99 +1,83 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimes, faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTimes,
+  faPlus,
+  faMinus,
+  faSmileBeam,
+} from "@fortawesome/free-solid-svg-icons";
 
-const ready = function (cb: any) {
-  // Check if the `document` is loaded completely
-  document.readyState === "loading"
-    ? document.addEventListener("DOMContentLoaded", function (e) {
-        cb();
-      })
-    : cb();
+type PlayerState = {
+  isAlive: boolean;
 };
+function App() {
+  const numPlayers = 10;
+  const [players, setPlayers] = useState<PlayerState[]>([]);
 
-let switchStatus = (el: any, reset = false) => {
-  let statusAlive = ["is-success", "is-light"];
-  let statusDead = "is-dark";
+  useEffect(() => {
+    console.log("rendering");
+    let ps: PlayerState[] = [];
+    [...Array(numPlayers)].forEach(() => {
+      ps.push({ isAlive: true });
+      // return <Player key={index} isAlive={true} />;
+      // return playerTemplate;
+    });
 
-  let alive = el.classList.contains(statusAlive[0]);
-  if (!alive || reset) {
-    el.classList.remove(statusDead);
-    el.classList.add(...statusAlive);
-    el.textContent = "Alive";
-  } else {
-    el.classList.add(statusDead);
-    el.classList.remove(...statusAlive);
-    el.textContent = "Dead";
-  }
-};
+    setPlayers(ps);
+  }, []);
 
-ready(function () {
-  let elems = document.querySelectorAll(".js-player");
-  elems.forEach((el) => {
-    let scoreEle = el.querySelector(".js-score");
-    let statusEl = el.querySelector(".js-status");
-    if (scoreEle === null || statusEl === null) {
+  let didBinds = useRef(false);
+  useEffect(() => {
+    if (didBinds.current) {
       return;
+    } else if (players.length > 0) {
+      didBinds.current = true;
     }
 
-    el.querySelector(".good")!.addEventListener("click", () => {
-      scoreEle!.innerHTML = (parseInt(scoreEle!.innerHTML) + 1).toString();
-    });
+    console.log("useEffect -> binding");
+    let elems = document.querySelectorAll(".js-player");
+    // console.log(elems)
+    elems.forEach((el) => {
+      let scoreEle = el.querySelector(".js-score");
+      let statusEl = el.querySelector(".js-status");
+      if (scoreEle === null || statusEl === null) {
+        // console.log("not found")
+        return;
+      }
+      // console.log("found")
 
-    el.querySelector(".bad")!.addEventListener("click", () => {
-      scoreEle!.innerHTML = (parseInt(scoreEle!.innerHTML) - 1).toString();
-    });
+      el.querySelector(".good")!.addEventListener("click", () => {
+        scoreEle!.innerHTML = (parseInt(scoreEle!.innerHTML) + 1).toString();
+      });
 
-    scoreEle!.addEventListener("click", () => {
-      scoreEle!.innerHTML = "0";
-    });
+      el.querySelector(".bad")!.addEventListener("click", () => {
+        scoreEle!.innerHTML = (parseInt(scoreEle!.innerHTML) - 1).toString();
+      });
 
-    statusEl.addEventListener("click", (e) => {
-      switchStatus(e.target);
+      scoreEle!.addEventListener("click", () => {
+        // console.log("scoreele")
+        scoreEle!.innerHTML = "0";
+      });
     });
   });
-});
 
-function App() {
-  const playerTemplate = (
-    <li>
-      <div className="field is-horizontal js-player">
-        <div className="field has-addons">
-          <div className="control">
-            <input
-              className="input player-name"
-              type="text"
-              placeholder="Player"
-            />
-          </div>
-          <div className="control player-score">
-            <button className="js-score button">0</button>
-          </div>
-        </div>
-        <div className="field ml-5">
-          <button className="button is-outlined is-danger bad">
-            <span className="icon is-medium">
-              <FontAwesomeIcon icon={faMinus} size="lg" />
-            </span>
-          </button>
-          <button className="button is-outlined is-success good">
-            <span className="icon is-medium">
-              <FontAwesomeIcon icon={faPlus} size="lg" />
-            </span>
-          </button>
-          <button className="status-btn button is-success is-light js-status">
-            Alive
-          </button>
-        </div>
-      </div>
-    </li>
-  );
+  let setAlive = (id: number, isAlive: boolean) => {
+    console.log("status updated");
+    players[id - 1].isAlive = isAlive;
+    setPlayers([...players]);
+  };
 
-  let playerEls = [...Array(10)].map((value: undefined, index: number) => {
-    // <Field id={index + 1} key={index} />
-    return playerTemplate;
+  let playerEls = players.map((player, index: number) => {
+    return (
+      <Player
+        key={index}
+        id={index + 1}
+        isAlive={player.isAlive}
+        onAliveChange={setAlive}
+      />
+    );
   });
 
   return (
@@ -105,6 +89,82 @@ function App() {
   );
 }
 
+type PlayerProps = {
+  isAlive: boolean;
+  onAliveChange: Function;
+  id: number;
+};
+
+class Player extends React.Component<PlayerProps> {
+  render() {
+    return (
+      <li>
+        <div className="field is-horizontal js-player">
+          <div className="field has-addons">
+            <div className="control">
+              <input
+                className="input player-name"
+                type="text"
+                placeholder="Player"
+              />
+            </div>
+            <div className="control player-score">
+              <button className="js-score button">0</button>
+            </div>
+          </div>
+          <div className="field ml-5">
+            <button className="button is-outlined is-danger bad">
+              <span className="icon is-medium">
+                <FontAwesomeIcon icon={faMinus} size="lg" />
+              </span>
+            </button>
+            <button className="button is-outlined is-success good">
+              <span className="icon is-medium">
+                <FontAwesomeIcon icon={faPlus} size="lg" />
+              </span>
+            </button>
+
+            <StatusButton
+              alive={this.props.isAlive}
+              onAliveChange={(isAlive: boolean) => {
+                this.props.onAliveChange(this.props.id, isAlive);
+              }}
+            />
+          </div>
+        </div>
+      </li>
+    );
+  }
+}
+
+type StatusButtonProps = { alive: boolean; onAliveChange: Function };
+const StatusButton = ({ alive, onAliveChange }: StatusButtonProps) => {
+  let btnClasses,
+    // icon,
+    btnText = null;
+  if (alive) {
+    btnClasses = "is-success is-light";
+    btnText = "Alive";
+  } else {
+    btnClasses = "is-dark";
+    btnText = "Dead";
+  }
+
+  return (
+    <button
+      className={`status-btn button js-status ${btnClasses}`}
+      onClick={() => {
+        onAliveChange(!alive);
+      }}
+    >
+      <span>{btnText}</span>
+      <span className="icon is-medium">
+        <FontAwesomeIcon icon={faSmileBeam} size="lg" />
+      </span>
+    </button>
+  );
+};
+
 const ResetButton = () => {
   const resetAll = () => {
     document.querySelectorAll(".js-score").forEach((e) => {
@@ -112,7 +172,7 @@ const ResetButton = () => {
     });
 
     document.querySelectorAll(".js-status").forEach((e) => {
-      switchStatus(e, true);
+      // switchStatus(e, true);
       return;
     });
   };
